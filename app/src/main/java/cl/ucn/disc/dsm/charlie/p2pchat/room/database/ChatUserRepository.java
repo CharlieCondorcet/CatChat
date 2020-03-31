470945025b80230f2a9731b4cede7c751d2c9b4d
charliecondorcet <charliecondorcet@gmail.com>
2020-03-31 19:15:05 -0300

    feat(ChatUserRepository): add ChatUser repository

---
+++ app/src/main/java/cl/ucn/disc/dsm/charlie/p2pchat/room/database/ChatUserRepository.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 CharlieCondorcet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.charlie.p2pchat.room.database;
+
+import android.app.Application;
+import androidx.lifecycle.LiveData;
+import androidx.room.RoomDatabase;
+import cl.ucn.disc.dsm.charlie.p2pchat.entities.ChatUser;
+import cl.ucn.disc.dsm.charlie.p2pchat.room.ChatUserDao;
+import java.util.List;
+
+/**
+ * @author Charlie Condorcet.
+ */
+public class ChatUserRepository {
+
+  private ChatUserDao mChatUserDao;
+  private LiveData<List<ChatUser>> mAllChatUsers;
+
+  // Note that in order to unit test the WordRepository, you have to remove the Application
+  // dependency. This adds complexity and much more code, and this sample is not about testing.
+  // See the BasicSample in the android-architecture-components repository at
+  // https://github.com/googlesamples
+  ChatUserRepository(Application application) {
+    ProyectRoomDatabase db = ProyectRoomDatabase.getDatabase(application);
+    mChatUserDao = db.chatUserDao();
+    mAllChatUsers = mChatUserDao.getAlphabetizedChatUsers();
+  }
+
+  // Room executes all queries on a separate thread.
+  // Observed LiveData will notify the observer when the data has changed.
+  LiveData<List<ChatUser>> getAllChatUsers() {
+    return mAllChatUsers;
+  }
+
+  // You must call this on a non-UI thread or your app will throw an exception. Room ensures
+  // that you're not doing any long running operations on the main thread, blocking the UI.
+  void insert(ChatUser chatUser) {
+    ProyectRoomDatabase.databaseWriteExecutor.execute(() -> {
+      mChatUserDao.insert(chatUser);
+    });
+  }
+
+}
