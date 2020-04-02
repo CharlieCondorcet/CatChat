9a80eaecf27945931f552f98020266e0e91234d9
charliecondorcet <charliecondorcet@gmail.com>
2020-04-02 15:17:44 -0300

    chore(AndroidManifest): update to define the new main activity

--- app/src/main/java/cl/ucn/disc/dsm/charlie/p2pchat/activities/LoginActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/charlie/p2pchat/activities/LoginActivity.java
@@ -51,7 +51,6 @@ public class LoginActivity extends AppCompatActivity {
 
     this.email = (EditText) findViewById(R.id.et_e_email);
     this.password = (EditText) findViewById(R.id.et_p_password);
-
   }
 
   public void verifyAccount(View view) {
