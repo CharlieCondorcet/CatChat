c23608ec23cca00c4e746a47fdf4d9916af38806
charliecondorcet <charliecondorcet@gmail.com>
2020-04-07 17:47:25 -0400

    test(ChatUserDaoTest): add unit test for each operation to ChatUser in DB

--- app/src/androidTest/java/cl/ucn/disc/dsm/charlie/p2pchat/ChatUserDaoTest.java
+++ app/src/androidTest/java/cl/ucn/disc/dsm/charlie/p2pchat/ChatUserDaoTest.java
@@ -12,17 +12,25 @@
 
 package cl.ucn.disc.dsm.charlie.p2pchat;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
 import android.content.Context;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import cl.ucn.disc.dsm.charlie.p2pchat.entities.ChatUser;
+import cl.ucn.disc.dsm.charlie.p2pchat.entities.Message;
 import cl.ucn.disc.dsm.charlie.p2pchat.room.ChatUserDao;
 import cl.ucn.disc.dsm.charlie.p2pchat.room.MessageDao;
 import cl.ucn.disc.dsm.charlie.p2pchat.room.database.ProyectRoomDatabase;
+import java.util.Date;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -56,4 +64,64 @@ public class ChatUserDaoTest {
     mDb.close();
   }
 
+  // Test that chatUsers can be added to a list in a newly created database.
+  @Test
+  public void insertAndGetMessage() throws Exception {
+    ChatUser user1 = new ChatUser(
+        "Goya",
+        "Francisco@Goya.com",
+        "ElAquelarre1798");
+    mChatUserDao.insert(user1);
+
+    List<ChatUser> allChatUsers = mChatUserDao.getAlphabetizedChatUsers();
+
+    // Newly added message at the start of the new list.
+    assertEquals(allChatUsers.get(0).getEmail(), user1.getEmail());
+  }
+
+  // Test to prove that the recovery of all ChatUsers is correct.
+  @Test
+  public void getAllChatUsers() throws Exception {
+    ChatUser user1 = new ChatUser(
+        "Camila",
+        "novacunes@tuhijo.cl",
+        "HolaHola123");
+    mChatUserDao.insert(user1);
+
+    ChatUser user2 = new ChatUser(
+        "Michael",
+        "notengocorreo@gmail.com",
+        "987654321abC");
+    mChatUserDao.insert(user2);
+
+    List<ChatUser> allChatUsers = mChatUserDao.getAlphabetizedChatUsers();
+
+    // The retrieval of messages by position is equal to the input of ChatUsers
+    // in the same position.
+    assertEquals(allChatUsers.get(0).getEmail(), user1.getEmail());
+    assertEquals(allChatUsers.get(1).getEmail(), user2.getEmail());
+  }
+
+  // Test to test the deletion of all ChatUsers that are created in the
+  // temporary database.
+  @Test
+  public void deleteAll() throws Exception {
+    ChatUser user1 = new ChatUser(
+        "Camila",
+        "novacunes@tuhijo.cl",
+        "HolaHola123");
+    mChatUserDao.insert(user1);
+
+    ChatUser user2 = new ChatUser(
+        "Michael",
+        "notengocorreo@gmail.com",
+        "987654321abC");
+    mChatUserDao.insert(user2);
+
+    mChatUserDao.deleteAll();
+    List<ChatUser> allChatUsers = mChatUserDao.getAlphabetizedChatUsers();
+
+    // it is verified that the list is empty after applying the elimination order.
+    assertTrue(allChatUsers.isEmpty());
+  }
 }
