732936efae4c8d1a8b407e35a0947af0a2b46053
charliecondorcet <charliecondorcet@gmail.com>
2020-04-07 18:14:43 -0400

    test(ConversationDaoTest): add the test fot each operation with Conversation

--- app/src/androidTest/java/cl/ucn/disc/dsm/charlie/p2pchat/ConversationDaoTest.java
+++ app/src/androidTest/java/cl/ucn/disc/dsm/charlie/p2pchat/ConversationDaoTest.java
@@ -12,17 +12,24 @@
 
 package cl.ucn.disc.dsm.charlie.p2pchat;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
 import android.content.Context;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import cl.ucn.disc.dsm.charlie.p2pchat.entities.ChatUser;
+import cl.ucn.disc.dsm.charlie.p2pchat.entities.Conversation;
 import cl.ucn.disc.dsm.charlie.p2pchat.room.ChatUserDao;
 import cl.ucn.disc.dsm.charlie.p2pchat.room.ConversationDao;
 import cl.ucn.disc.dsm.charlie.p2pchat.room.database.ProyectRoomDatabase;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -54,5 +61,60 @@ public class ConversationDaoTest {
     mDb.close();
   }
 
+  // Test that Conversation can be added to a list in a newly created database.
+  @Test
+  public void insertAndGetMessage() throws Exception {
+    Conversation conversation1 = new Conversation(
+        4,
+        1);
+    mConversationDao.insert(conversation1);
+
+    List<Conversation> allConversations = mConversationDao.getAllConversation();
+
+    // Newly added conversation at the start of the new list.
+    assertEquals(allConversations.get(0).getIdConversation(), conversation1.getIdConversation());
+  }
+
+  // Test to prove that the recovery of all Conversation is correct.
+  @Test
+  public void getAllChatUsers() throws Exception {
+    Conversation conversation1 = new Conversation(
+        4,
+        1);
+    mConversationDao.insert(conversation1);
+
+    Conversation conversation2 = new Conversation(
+        1,
+        3);
+    mConversationDao.insert(conversation2);
+
+    List<Conversation> allConversations = mConversationDao.getAllConversation();
+
+    // The retrieval of conversation by position is equal to the input of
+    // Conversations list in the same position.
+    assertEquals(allConversations.get(0).getIdFirstUser(), conversation1.getIdFirstUser());
+    assertEquals(allConversations.get(1).getIdSecondUser(), conversation2.getIdSecondUser());
+  }
+
+  // Test to test the deletion of all Conversation that are created in the
+  // temporary database.
+  @Test
+  public void deleteAll() throws Exception {
+    Conversation conversation1 = new Conversation(
+        4,
+        1);
+    mConversationDao.insert(conversation1);
+
+    Conversation conversation2 = new Conversation(
+        1,
+        3);
+    mConversationDao.insert(conversation2);
+
+    mConversationDao.deleteAll();
+    List<Conversation> allConversations = mConversationDao.getAllConversation();
+
+    // it is verified that the list is empty after applying the elimination order.
+    assertTrue(allConversations.isEmpty());
+  }
 
 }
